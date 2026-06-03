74b08e19dedf2acda2ab4903639e1bcea189a7e3
S-16ahana <sahanakadrolli16@gmail.com>
2026-06-03 12:13:20 +0530

    Initial JavaScript practice commit

---
+++ Phase1/script.js
@@ -0,0 +1,64 @@
+// ## Console & Basics
+// 1. Print `"Hello JavaScript"` in the console.
+// console.log("Hello Javascript")
+
+// // 2. Print your name, age, and city using one `console.log()`.
+// var name='sahana'
+// var age = 24
+// var city ='Dharwad'
+// console.log(`my name is ${name} and age is ${age} and city is ${city}`)
+
+// // 3. Print a warning message using `console.warn()`.
+// console.warn("This is a warning message babe")
+
+// // 4. Print an error message using `console.error()`.
+// console.error("This is an Error message babe")
+
+// // 5. Use `console.table()` to display an array of 5 numbers.
+// console.table([1,2,3,4,5,6])
+
+// // ## Variables
+
+// // 1. Create a variable called `studentName` and store your name in it.
+// var studentName = 'sahana'
+
+// // 2. Create a variable `age` and print it.
+// var age = 24
+// console.log(age);
+
+// 3. Create two variables and swap their values.
+// var a=24
+// var b=34
+// var temp = a
+// a=b
+// b=temp
+// console.log(a)
+// console.log(b)
+
+// OR
+
+// var a = 24;
+// var b = 34;
+
+// [a, b] = [b, a];
+
+// console.log(a); // 34
+// console.log(b); // 24
+// // 4. Create a constant variable for `PI` and print it.
+// const PI = 3.17;
+// console.log(PI);
+
+// // 5. Declare a variable without assigning a value and print it.
+// var t;
+// console.log(t);  //undefined
+
+// // 6. Create a variable `score` and increase it by 10.
+// var score =1;
+// score+=10;
+// console.log(score)
+
+// // 7. Create three variables for first name, last name, and full name.
+// var firstName = 'Sahana'
+// var lastName ='Kadrolli'
+// var fullName = firstName+""+lastName
+// console.log(fullName)
